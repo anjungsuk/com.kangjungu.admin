fb8947f4201dcdb1421107b5dd583f62774acf94
anjugnsuk <kjg0456@gmail.com>
2021-03-02 13:42:42 +0900

    네번째 신규 커밋 2021.03.02

--- src/main/java/admin/kangjungu/com/Application.java
+++ src/main/java/admin/kangjungu/com/Application.java
@@ -2,8 +2,12 @@ package admin.kangjungu.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 //SpringBootApplication 어노테이션을 통한 스프링부트 자동설정, Bean 읽기와 생성을 자동으로 한다.
 @SpringBootApplication
+//Jpa Auditing 활성화
+@EnableJpaAuditing
 //프로젝트 메인 클래스
 public class Application
 {
--- src/test/java/admin/kangjungu/com/web/domain/posts/PostsRepositoeyTest.java
+++ src/test/java/admin/kangjungu/com/web/domain/posts/PostsRepositoeyTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +42,24 @@ public class PostsRepositoeyTest
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+    @Test
+    public void BaseTimeEntity_test()
+    {
+        LocalDateTime now = LocalDateTime.of(2021, 2, 26, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        //debug
+        System.out.println(">>>test>>>> 생성일 : " + posts.getCreatedDate()+"modifiedDate ="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
